bf57deb1dbe6d325a5ccb09c5df1cf3a2526fbd4
shigri313 <imshigri110@gamil.com>
2024-04-02 08:08:10 +0500

    adding a Pc password

---
+++ main.js
@@ -0,0 +1,16 @@
+#! /usr/bin/evn node
+let password = "shigri313";
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        message: "Enter your password ",
+        type: "string",
+        name: "user",
+    },
+]);
+if (answer.user != password) {
+    console.log("please Enter a valid password ");
+}
+else {
+    console.log("Congratulation , you enter the right password");
+}
